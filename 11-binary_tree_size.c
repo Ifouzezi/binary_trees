3c7f4f283d9a4f03f65e99ddd51028f46cdd4d22
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-31 12:30:05 +0100

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,10 +2,11 @@
 
 /**
  * binary_tree_size - Measures the size of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the size of.
+ * @tree: A pointer to the root node.
  *
  * Return: The size of the tree.
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;

c655be1fd93a1508f98e1f0ea617b052be614c9a
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-31 12:59:32 +0100

    Update 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -3,10 +3,11 @@
 
 /**
  * binary_tree_rotate_left - Left-rotates a binary tree.
- * @tree: A pointer to the root node of the tree to rotate.
+ * @tree: A pointer to the root node.
  *
  * Return: A pointer to the new root node after rotation.
  */
+
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *pivot, *tmp;
